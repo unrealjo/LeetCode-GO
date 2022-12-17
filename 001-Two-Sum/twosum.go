8c632a463ab0ca4d658c2b487076399c567555a8
Youssef Eddaif <jo9unreal@gmail.com>
2022-12-17 21:15:55 +0100

    Fix and update problem 001

--- 001-Two-Sum/twosum.go
+++ 001-Two-Sum/twosum.go
@@ -1,13 +1,19 @@
 package twosum
 
+/*
+01-Given an array of integers `nums` and an integer `target`,
+return indices of the two numbers such that they add up to `target.
+*/
 func twoSum(nums []int, target int) []int {
-	l := len(nums)
-	for index, n := range nums {
-		for i := index; i < l; i++ {
-			if nums[i]+n == target {
-				return []int{index, i}
-			}
+	var c int
+	dics := make(map[int]int)
+	for i, v := range nums {
+		c = target - v
+		_, ok := dics[c]
+		if ok {
+			return []int{dics[c], i}
 		}
+		dics[v] = i
 	}
 	return []int{}
 }
