532e16bb4388b4cf048f87142ef774b99dacb5c3
Youssef Eddaif <jo9unreal@gmail.com>
2022-12-17 21:15:19 +0100

    Update testing for problem 001

--- 001-Two-Sum/twosum_test.go
+++ 001-Two-Sum/twosum_test.go
@@ -5,14 +5,22 @@ import (
 	"testing"
 )
 
+type Testcase struct {
+	nums   []int
+	target int
+	expect []int
+}
+
 func TestTwoSum(t *testing.T) {
-	nums := []int{2, 7, 11, 15}
-
-	if !reflect.DeepEqual(twoSum(nums, 9), []int{0, 1}) {
-		t.Error("Failed, two sum")
+	ans := []Testcase{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 5, -3}, 0, []int{0, 2}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{3, 3}, 6, []int{0, 1}},
 	}
-
-	if !reflect.DeepEqual(twoSum(nums, 6), []int{}) {
-		t.Error("Failed, two sum")
+	for index, an := range ans {
+		if !reflect.DeepEqual(twoSum(an.nums, an.target), an.expect) {
+			t.Errorf("Wrong result at index %d", index)
+		}
 	}
 }
