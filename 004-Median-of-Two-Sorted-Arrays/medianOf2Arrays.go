a8b58dac493c4f9d7af060834007a9a28f35b072
Youssef Eddaif <jo9unreal@gmail.com>
2022-12-24 12:26:02 +0100

    Refactor 004

--- 004-Median-of-Two-Sorted-Arrays/medianOf2Arrays.go
+++ 004-Median-of-Two-Sorted-Arrays/medianOf2Arrays.go
@@ -11,19 +11,19 @@ func findMedianSortedArrays(a, b []int) float64 {
 		list = append(list, float64(v))
 	}
 	sort.Float64s(list)
-	l := len(list)
+	listMiddle := len(list) / 2
 
 	/*
 		Note : number elements is n but in the implementation is equal to l - 1
 		If the total number elements given is odd, then the median is:
 		median = [(n/2)th element + (n/2)th element] / 2
 	*/
-	if l%2 == 0 {
-		return (list[l/2] + list[l/2-1]) / 2.0
+	if listMiddle%2 == 0 {
+		return (list[listMiddle] + list[listMiddle-1]) / 2.0
 	}
 	/*
 		If the total number elements given is odd, then the median is:
 		median = (n+1/2)th element
 	*/
-	return float64(list[l/2])
+	return list[listMiddle]
 }
