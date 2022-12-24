8155025ff78c26d30ed4e835ba4988f0ec108497
Youssef Eddaif <jo9unreal@gmail.com>
2022-12-24 12:26:29 +0100

    Refactor 006

--- 006-Zigzag-Conversion/zigzagConversion.go
+++ 006-Zigzag-Conversion/zigzagConversion.go
@@ -8,18 +8,18 @@ func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
-	var res strings.Builder
-	l := len(s)
+	var result strings.Builder
+	listLength := len(s)
 	step := (numRows - 1) * 2
 	for i := 0; i < numRows; i++ {
 		// reset steps at last row
-		for k := i; k < l; k += step { 
-			res.WriteByte(s[k])
-			if i < numRows-1 && i > 0 && k+step-i*2 < l {
-				res.WriteByte(s[step+k-i*2])
+		for k := i; k < listLength; k += step {
+			result.WriteByte(s[k])
+			if i < numRows-1 && i > 0 && k+step-i*2 < listLength {
+				result.WriteByte(s[step+k-i*2])
 			}
 
 		}
 	}
-	return res.String()
+	return result.String()
 }
