b0bb806d38b460f762da038c9c97cbef2f7582ed
Youssef Eddaif <jo9unreal@gmail.com>
2022-12-29 21:13:06 +0100

    Update 009 : Adding more comments

--- 009-PalindromeNumber/palindromeNumber.go
+++ 009-PalindromeNumber/palindromeNumber.go
@@ -3,7 +3,11 @@ package palindromenumber
 func isPalindrome(x int) bool {
 	k, reversedX := 1, 0
 	sign := 1
-	// check for negativity
+	/*
+		check for negativity then
+		Get only the absolute value of x
+		if x < 0 then multiply x by -1 otherwise do nothing
+	*/
 	if x < 0 {
 		sign = -1
 		x = x * -1
